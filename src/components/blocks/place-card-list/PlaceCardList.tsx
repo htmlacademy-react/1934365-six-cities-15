2d5a470d17dd78ed7d7d6a8101806624ff22b4b9
unknown <natapolyakova13@gmail.com>
2024-02-24 12:59:30 +0300

    fix: исправляет баги по комментариям наставника

--- src/components/blocks/place-card-list/PlaceCardList.tsx
+++ src/components/blocks/place-card-list/PlaceCardList.tsx
@@ -2,19 +2,15 @@ import { PlaceCardPropsType } from '../../utils/mocks';
 
 import PlaceCard from '../place-card/PlaceCard';
 
-export default function PlaceCardList(props: { places: Array<PlaceCardPropsType>}): JSX.Element {
+export default function PlaceCardList(props: { places: Array<PlaceCardPropsType> }): JSX.Element {
   return (
-    props.places.map((el: PlaceCardPropsType) => (
-      <PlaceCard
-        className = {el.className}
-        key={el.id}
-        title = {el.title}
-        type = {el.type}
-        price = {el.price}
-        previewImage = {el.previewImage}
-        rating = {el.rating}
-        isPremium = {el.isPremium}
-      />
-    ))
+    <>
+      {props.places.map((card: PlaceCardPropsType) => (
+        <PlaceCard
+          key = {card.id}
+          card = {card}
+        />
+      ))}
+    </>
   );
 }
--- src/components/blocks/place-card/PlaceCard.tsx
+++ src/components/blocks/place-card/PlaceCard.tsx
@@ -1,18 +1,18 @@
 import { PlaceCardPropsType } from '../../utils/mocks';
 
-export default function PlaceCard({ title, type, price, previewImage, isPremium, className, width, height }: PlaceCardPropsType): JSX.Element {
+export default function PlaceCard({ card }: {card: PlaceCardPropsType}): JSX.Element {
   return (
-    <article className={`${className}__card place-card`}>
-      {isPremium ? <div className="place-card__mark"><span>Premium</span></div> : ''}
-      <div className={`${className}__image-wrapper place-card__image-wrapper"`}>
+    <article className={`${card.className}__card place-card`}>
+      {card.isPremium ? <div className="place-card__mark"><span>Premium</span></div> : ''}
+      <div className={`${card.className}__image-wrapper place-card__image-wrapper"`}>
         <a href="#">
-          <img className="place-card__image" src={previewImage} width={width} height={height} alt="Place image" />
+          <img className="place-card__image" src={card.previewImage} width={card.width} height={card.height} alt="Place image" />
         </a>
       </div>
-      <div className={`${className}__card-info place-card__info"`}>
+      <div className={`${card.className}__card-info place-card__info"`}>
         <div className="place-card__price-wrapper">
           <div className="place-card__price">
-            <b className="place-card__price-value">&euro;{price}</b>
+            <b className="place-card__price-value">&euro;{card.price}</b>
             <span className="place-card__price-text">&#47;&nbsp;night</span>
           </div>
           <button className="place-card__bookmark-button button" type="button">
@@ -29,9 +29,9 @@ export default function PlaceCard({ title, type, price, previewImage, isPremium,
           </div>
         </div>
         <h2 className="place-card__name">
-          <a href="#">{title}</a>
+          <a href="#">{card.title}</a>
         </h2>
-        <p className="place-card__type">{type}</p>
+        <p className="place-card__type">{card.type}</p>
       </div>
     </article>
   );
