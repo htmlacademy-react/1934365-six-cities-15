82201802b21054d4f1892e8d8a1e1a3d004939ec
unknown <natapolyakova13@gmail.com>
2024-03-02 10:16:08 +0300

    feat: добавляет Link

--- src/components/blocks/place-card/PlaceCard.tsx
+++ src/components/blocks/place-card/PlaceCard.tsx
@@ -1,6 +1,8 @@
+import { Link } from 'react-router-dom';
 import { PlaceCardPropsType } from '../../utils/mocks';
+import { AppRoute } from '../../utils/types';
 
-export default function PlaceCard({ card }: {card: PlaceCardPropsType}, onCardHover: (card?: PlaceCardPropsType) => void): JSX.Element {
+export default function PlaceCard({ card }: { card: PlaceCardPropsType }, onCardHover: (card?: PlaceCardPropsType) => void): JSX.Element {
   console.log(onCardHover)
   const onMouseOn = () => {
     onCardHover(card)
@@ -11,38 +13,40 @@ export default function PlaceCard({ card }: {card: PlaceCardPropsType}, onCardHo
   }
 
   return (
-    <article className={`${card.className}__card place-card`} onMouseEnter={onMouseOn} onMouseLeave={onMouseOff}>
-      {card.isPremium ? <div className="place-card__mark"><span>Premium</span></div> : ''}
-      <div className={`${card.className}__image-wrapper place-card__image-wrapper"`}>
-        <a href="#">
-          <img className="place-card__image" src={card.previewImage} width={card.width} height={card.height} alt="Place image" />
-        </a>
-      </div>
-      <div className={`${card.className}__card-info place-card__info"`}>
-        <div className="place-card__price-wrapper">
-          <div className="place-card__price">
-            <b className="place-card__price-value">&euro;{card.price}</b>
-            <span className="place-card__price-text">&#47;&nbsp;night</span>
-          </div>
-          <button className="place-card__bookmark-button button" type="button">
-            <svg className="place-card__bookmark-icon" width="18" height="19">
-              <use xlinkHref="#icon-bookmark"></use>
-            </svg>
-            <span className="visually-hidden">To bookmarks</span>
-          </button>
+    <Link to={AppRoute.Offer}>
+      <article className={`${card.className}__card place-card`} onMouseEnter={onMouseOn} onMouseLeave={onMouseOff}>
+        {card.isPremium ? <div className="place-card__mark"><span>Premium</span></div> : ''}
+        <div className={`${card.className}__image-wrapper place-card__image-wrapper"`}>
+          <a href="#">
+            <img className="place-card__image" src={card.previewImage} width={card.width} height={card.height} alt="Place image" />
+          </a>
         </div>
-        <div className="place-card__rating rating">
-          <div className="place-card__stars rating__stars">
-            <span style={{ width: '80%' }}></span>
-            <span className="visually-hidden">Rating</span>
+        <div className={`${card.className}__card-info place-card__info"`}>
+          <div className="place-card__price-wrapper">
+            <div className="place-card__price">
+              <b className="place-card__price-value">&euro;{card.price}</b>
+              <span className="place-card__price-text">&#47;&nbsp;night</span>
+            </div>
+            <button className="place-card__bookmark-button button" type="button">
+              <svg className="place-card__bookmark-icon" width="18" height="19">
+                <use xlinkHref="#icon-bookmark"></use>
+              </svg>
+              <span className="visually-hidden">To bookmarks</span>
+            </button>
           </div>
+          <div className="place-card__rating rating">
+            <div className="place-card__stars rating__stars">
+              <span style={{ width: '80%' }}></span>
+              <span className="visually-hidden">Rating</span>
+            </div>
+          </div>
+          <h2 className="place-card__name">
+            <a href="#">{card.title}</a>
+          </h2>
+          <p className="place-card__type">{card.type}</p>
         </div>
-        <h2 className="place-card__name">
-          <a href="#">{card.title}</a>
-        </h2>
-        <p className="place-card__type">{card.type}</p>
-      </div>
-    </article>
+      </article>
+    </Link>
   );
 }
 
