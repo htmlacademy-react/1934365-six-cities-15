a12dd93a43c97fcf541ebe78a26e257eb7051a10
unknown <natapolyakova13@gmail.com>
2024-04-12 09:05:24 +0300

    chore: убирает ошибки при билде

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,22 +1,22 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { AppRoute, RequestStatus } from '../utils/types';
-import { AppPropsType } from './types';
-import NotFound from '../../pages/not-found-page/not-found';
-import PrivateRoute from '../blocks/private-route/private-route';
+import { AppRoute, RequestStatus } from '../utils/types.ts';
+import { AppPropsType } from './types.ts';
+import NotFound from '../../pages/not-found-page/not-found.tsx';
+import PrivateRoute from '../blocks/private-route/private-route.tsx';
 import { HelmetProvider } from 'react-helmet-async';
 import Layout from '../layout/layout/layout.tsx';
-import OfferPage from '../../pages/offer-page/offer-page';
-import FavoritesPage from '../../pages/favorites-page/favorites-page';
-import MainPage from '../../pages/main-page/main-page';
-import { useActionCreators, useAppSelector } from '../../store/hooks';
+import OfferPage from '../../pages/offer-page/offer-page.tsx';
+import FavoritesPage from '../../pages/favorites-page/favorites-page.tsx';
+import MainPage from '../../pages/main-page/main-page.tsx';
+import { useActionCreators, useAppSelector } from '../../store/hooks.ts';
 import { useEffect } from 'react';
-import { offersActions } from '../../store/slices/offers';
-import { userActions, userSliceSelectors } from '../../store/slices/user';
-import { getToken } from '../../services/token';
-import LoginPage from '../../pages/login-page/login-page';
+import { offersActions } from '../../store/slices/offers.ts';
+import { userActions, userSliceSelectors } from '../../store/slices/user.ts';
+import { getToken } from '../../services/token.ts';
+import LoginPage from '../../pages/login-page/login-page.tsx';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import Loader from '../ui/loader/loader';
+import Loader from '../ui/loader/loader.tsx';
 
 export default function App({ cities }: AppPropsType): JSX.Element {
   const { fetchAllOffers } = useActionCreators(offersActions);
