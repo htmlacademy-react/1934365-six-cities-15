530fde701d90cfcc032daf4576a49b34a725cb01
unknown <natapolyakova13@gmail.com>
2024-04-12 09:11:32 +0300

    chore: убирает ошибки при билде

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/app/app';
 import { CITIES } from './components/utils/constants';
 import { Provider } from 'react-redux';
 import { store } from './store';
+import App from './components/app/app';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
